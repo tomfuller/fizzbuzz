1fe6ec9e233fda483f05f8c42b4902278401727a
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-02 16:48:13 +0000

    finished fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,15 @@
 def fizzbuzz(number)
+m1 = number % 3 == 0
+m2 = number % 5 == 0
+m3 = number % 15 == 0
 
-'fizz'
-
-
+  if m3 #number % 15 == 0
+    'fizzbuzz'
+  elsif m1 #number % 3 == 0
+    'fizz'
+  elsif m2 #number % 5 == 0
+    'buzz'
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,36 @@
 require './lib/fizzbuzz'
-
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'return "buzz" when passed 5' do
+   expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'return fizzbuzz when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'return fizz when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'return number when passed 8' do
+    expect(fizzbuzz(8)).to eq 8
+  end
+end
+describe 'fizzbuzz' do
+  it 'return buzz when passed 25' do
+    expect(fizzbuzz(25)).to eq "buzz"
+  end
+end
+describe 'fizzbuzz' do
+  it 'return fizzbuzz when passed 45' do
+    expect(fizzbuzz(45)).to eq "fizzbuzz"
   end
 end
