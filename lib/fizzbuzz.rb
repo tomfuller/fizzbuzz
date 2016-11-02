2d1a1172447469dd2f2a27d89fd5b8190fb6b5be
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-02 16:50:04 +0000

    fizzbuzz refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,11 +3,11 @@ m1 = number % 3 == 0
 m2 = number % 5 == 0
 m3 = number % 15 == 0
 
-  if m3 #number % 15 == 0
+  if m3
     'fizzbuzz'
-  elsif m1 #number % 3 == 0
+  elsif m1
     'fizz'
-  elsif m2 #number % 5 == 0
+  elsif m2
     'buzz'
   else
     number
